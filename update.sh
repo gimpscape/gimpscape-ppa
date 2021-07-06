75387f1d75166f1661f6ebb9b6048fd005957f90
Rania Amina <reaamina@gmail.com>
2021-07-06 08:01:04 +0700

    tes multidistro repo

--- update.sh
+++ update.sh
@@ -22,6 +22,46 @@ apt-ftparchive release . > Release
 gpg --default-key "me@raniaaamina.id" -abs -o - Release > Release.gpg
 gpg --default-key "me@raniaamina.id" --clearsign -o - Release > InRelease
 
+# add Release for Debian buster
+cd ../buster
+
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+
+apt-ftparchive release . > Release
+gpg --default-key "me@raniaaamina.id" -abs -o - Release > Release.gpg
+gpg --default-key "me@raniaamina.id" --clearsign -o - Release > InRelease
+
+# add Release for Debian Bullseye
+cd ../bullseye
+
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+
+apt-ftparchive release . > Release
+gpg --default-key "me@raniaaamina.id" -abs -o - Release > Release.gpg
+gpg --default-key "me@raniaamina.id" --clearsign -o - Release > InRelease
+
+# add Release for Debian Bionic
+cd ../bionic
+
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+
+apt-ftparchive release . > Release
+gpg --default-key "me@raniaaamina.id" -abs -o - Release > Release.gpg
+gpg --default-key "me@raniaamina.id" --clearsign -o - Release > InRelease
+
+# add Release for Debian Focal
+cd ../focal
+
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+
+apt-ftparchive release . > Release
+gpg --default-key "me@raniaaamina.id" -abs -o - Release > Release.gpg
+gpg --default-key "me@raniaamina.id" --clearsign -o - Release > InRelease
+
 # Commit & push
 git add -A
 git commit -m "$MESSAGE"
